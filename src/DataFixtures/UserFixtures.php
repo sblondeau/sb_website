855212704a8f5769366e89e4be26fbe893758358
Sylvain Blondeau <contact@sylvainblondeau.dev>
2023-09-06 11:54:16 +0200

    add authentication

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {}
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setEmail('admin@sylvainblondeau.dev');
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'admin'));
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+        $manager->flush();
+    }
+}
