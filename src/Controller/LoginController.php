a1ccb9a2fc282751afa8b1d85d15d51c751f9b05
Sylvain Blondeau <contact@sylvainblondeau.dev>
2023-09-13 12:00:24 +0200

    fix login and remove duplicate headears

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,9 +12,9 @@ class LoginController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app_admin');
-        }
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('app_admin');
+        // }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
