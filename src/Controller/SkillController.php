25f333352b6d78cebd9a016e4f66888330864157
Sylvain Blondeau <contact@sylvainblondeau.dev>
2023-11-13 16:22:17 +0100

    update cv and social links

--- src/Controller/SkillController.php
+++ src/Controller/SkillController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
-use App\Repository\SkillCategoryRepository;
 use App\Repository\SkillRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\SkillCategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SkillController extends AbstractController
 {
+    public const CV_PATH_NAME = 'CV_Sylvain_Blondeau_Freelance.pdf';
+
     #[Route('/mes-competences', name: 'app_skill')]
     public function index(SkillCategoryRepository $skillCategoryRepository): Response
     {
@@ -17,5 +21,16 @@ class SkillController extends AbstractController
         return $this->render('skill/index.html.twig', [
             'skillCategories' => $skillCategories,
         ]);
+    }
+
+    #[Route('/cv', name: 'app_cv')]
+    public function cv()
+    {
+        $response =  new BinaryFileResponse('../assets/files/' . self::CV_PATH_NAME);
+        $response->setContentDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            self::CV_PATH_NAME
+        );
+        return $response;
     }
 }
